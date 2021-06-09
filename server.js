f702935d74855227c20afff5e256c5cc2612cd91
uday bhaskhar <udaybhaskhar@udays-Mac-mini.local>
2021-06-09 23:39:14 +0530

    feat(server): add git auth

--- server.js
+++ server.js
@@ -2,43 +2,40 @@ const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 const shortid = require('shortid');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github').Strategy;
 
-// passport.use(new GitHubStrategy({
-//     clientID: '6bcaff828e7c309ffd2d',
-//     clientSecret: '84236d72696f19991d28f42a87e8957882649bfe',
-//     callbackURL: "/auth/github/callback"
-//   },
-//   function(accessToken, refreshToken, profile, cb) {
-//       console.log({ profile, accessToken, refreshToken });
-//     // User.findOrCreate({ githubId: profile.id }, function (err, user) {
-//       return cb(err,{ profile, accessToken, refreshToken });
-//     // });
-//   }
-// ));
-
- const data = {
-    clientID: '6bcaff828e7c309ffd2d',
-    clientSecret: '84236d72696f19991d28f42a87e8957882649bfe',
-    callbackURL: "/auth/github/callback",
-    scopes: 'repo'
-  };
-
-app.get('/auth/github', (req, res) => {
-  fetch(`
-  https://github.com/login/oauth/authorize?client_id=${data.clientID}&redirect_uri=${data.callbackURL}&scopes=${data.scopes}
-  &state=${shortid.generate()}
-  `)
-    .then(res => {
-      console.log(res);
-    })
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested With, Content-Type, Accept');
+  next();
 });
 
-// app.get('/auth/github/callback', 
-//   passport.authenticate('github', { failureRedirect: '/login' }),
-//   function(req, res) {
-//     // Successful authentication, redirect home.
-//     res.redirect('/');
-// });
+passport.use(new GitHubStrategy({
+    clientID: '6bcaff828e7c309ffd2d',
+    clientSecret: '84236d72696f19991d28f42a87e8957882649bfe',
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log({ profile, accessToken, refreshToken });
+    // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return cb({ profile, accessToken, refreshToken });
+    // });
+  }
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log('redirected');
+    res.redirect('http://localhost:9000/#/login');
+});
 
 
 
